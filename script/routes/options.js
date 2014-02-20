d0332a7aca6fa5b319c815e6a3c9ccb10d61e3b3
Fabrice Weinberg <Fabrice@weinberg.me>
2014-02-20 10:56:08 +0100

    Add options route
    This route will contain the metadata about the default-/max Value of
    each options that can be specified in the url

---
+++ script/routes/options.js
@@ -0,0 +1,10 @@
+
+module.exports = function (app, config){
+
+  app.get('/options', function(req, res, next){
+
+    res.writeHead(200, {'Content-Type' : 'application/json'});
+    res.end(JSON.stringify(config.screenshot.url));
+  });
+
+};
