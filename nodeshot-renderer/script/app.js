8549bd4cb7c4e4d1a17335c41b030c232124a42b
Fabrice Weinberg <Fabrice@weinberg.me>
2014-02-23 19:47:07 +0100

    globaltimeout to cancel request if it takes to long

--- nodeshot-renderer/script/app.js
+++ nodeshot-renderer/script/app.js
@@ -1,12 +1,21 @@
 var ScreenshotApi = require('./lib/ScreenshotApi.js'),
 
     fs            = require('fs'),
+    path          = require('path'),
 
     winston       = require('winston'),
     config        = require('config'),
 
     kue           = require('kue'),
     jobs          = kue.createQueue();
+
+var cwd = process.cwd();
+
+var configFolder = path.resolve(cwd, config.folder);
+// Create the temp directory
+if (!fs.existsSync(configFolder)){
+  winston.info('"%s" folder not found. Make sure the server configuration is correct.', configFolder);
+}
 
 
 if ( !!config.logging )
@@ -18,19 +27,30 @@ var screenshotApi = new ScreenshotApi(config.screenshot);
 
 jobs.process('screenshot', function(job, done){
   winston.info('Process job "%s"', job.data.title);
-    try{
-    screenshotApi.screenshot(job.data.title, job.data.options, job, function(err, stream){
-      if ( err ){
-        job.log( '' + err );
 
-        done(err);
+  var canceled = false,
+      globalTimeout = setTimeout(function(){
+        canceled = true;
+        done ( new Error('Request took longer than ' + config.screenshot.globaltimeout + 'ms') );
+      }, config.screenshot.globaltimeout);
+
+    try{
+
+    screenshotApi.screenshot(job.data.title, job.data.options, job, function(err, stream){
+      clearTimeout(globalTimeout);
+      if ( canceled ) return;
+
+      if ( err ){
+
+        job.log( '' + err );
+        done ( err );
 
       } else {
 
         job.log('Cache Request ' + job.data.id);
         job.log('Done');
 
-        stream.pipe(fs.createWriteStream(config.cache.folder + '/' + job.data.id));
+        stream.pipe(fs.createWriteStream(path.resolve(cwd, config.cache.folder, job.data.id)));
         done();
 
       }
