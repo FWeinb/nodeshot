aea3dddef576a5a2500a4c9ba8d5d84f1f91c67f
Fabrice Weinberg <Fabrice@weinberg.me>
2014-02-24 21:21:59 +0100

    Add ajax route. Cleaned up. Better error handling.

--- nodeshot-server/routes/index.js
+++ nodeshot-server/routes/index.js
@@ -1,192 +1,176 @@
 var  utils         = require('./../lib/utils.js'),
+     buildOptions  = require('./../lib/buildOptions.js'),
      fs            = require('fs'),
      winston       = require('winston'),
      validator     = require('validator'),
      kue           = require('kue'),
      jobs          = kue.createQueue();
 
-// Hacky. This will hold job objects that are currently running.
-var pendingJobs = {};
+
+module.exports = function (app, config, pendingJobs, cacheService){
 
 
-module.exports = function (app, config, cacheService){
+  var normalizeReq = function ( req, res, next ){
+      if (!req.query.url) {
 
-  /**
-   * Build an options object from the request query.
-   */
-  var numOptions  = ['width', 'height', 'delay'],
-      boolOptions = ['fullpage', 'scrollbar', 'force'],
-      strOptions  = ['format', 'callback'];
+        throw new Error('At least the URL param must be set.');
 
-  var buildOptions = function(query, defaults){
-    var options = {};
+      } else {
 
-    // Add numOptions
-    numOptions.forEach(function(item){
-      if (!!query[item]){
-        var value = parseInt(query[item], 10);
-        // Check if limit are fullfiled
-        if ( 0 < value && value <= defaults['max' + item] ){
-          options[item] = value;
-        }else{
-          throw new Error(item + ' must be bigger than 0 and lesser or equal to ' + defaults['max' + item]);
+        var url = req.query.url;
+
+        // Assume http:// if nothing is specified.
+        if ( url.indexOf('http') === -1 ){
+          url = 'http://' + url;
         }
-      }
-    });
 
-    // Add strOptions
-    strOptions.forEach(function(item){
-      if (!!query[item]){
-        if (!!defaults['allowed' + item] || defaults['allowed' + item].indexOf(query[item]) != -1){
-          options[item] = query[item];
-        } else{
-          throw new Error( item + ' must be one of these values: ' + defaults['allowed' + item]);
+        if (!validator.isURL(url)){
+          throw new Error('Invalid URL: '+ url);
         }
-      }
+
+        // Build options from request.
+        var options   = buildOptions(req.query, config.screenshot);
+        var optionStr = JSON.stringify(options);
+
+        // Create a uniqueID from url, options and the imageId format
+        var imageId = utils.md5(url + optionStr) + "." + options.format;
+
+        winston.info('Request "%s", cacheId: "%s"', url, imageId, optionStr);
+
+
+        // &force will invalidate the cache
+        if ( options.force ){
+          winston.info('Remove "%s" from cache', imageId);
+          cacheService.removeFile(imageId);
+        }
+
+        req.screenshot = {
+          url     : url,
+          options : options,
+          imageId : imageId
+        };
+
+        // Execute next
+        next();
+     }
+  };
+
+
+
+  var attachListenerTo = function(job, imageId, serveImageCallback){
+    job.on('complete', function(){
+      // remove from pending job list.
+      delete pendingJobs[imageId];
+
+      winston.info("server new image");
+      cacheService.addFile(imageId);
+
+      serveImageCallback(null, imageId);
     });
 
-    // Add boolOptions
-    boolOptions.forEach(function(item){
-      if (!!query[item]){
-        options[item] = false;
-        switch(query[item].toLowerCase()){
-            case "true":
-            case "yes":
-            case "1":
-              options[item] = true;
-          }
-      }
+    job.on('failed', function(){
+      kue.Job.get(job.id, function(err, job){
+        if ( err ){
+          serveImageCallback(err, null);
+          return;
+        }
+        serveImageCallback(job._error, null);
+      });
+      // remove from pending job list.
+      delete pendingJobs[imageId];
     });
+  };
 
+  var createOrAttachToJob = function ( url, imageId, options, serveImageCallback ){
 
-    // Merge in defaults
-    options.format    = options.format    || defaults.format;
-    options.scrollbar = options.scrollbar || defaults.scrollbar;
-    options.fullpage  = options.fullpage  || defaults.fullpage;
-    options.delay     = options.delay     || defaults.delay;
-    options.width     = options.width     || defaults.width;
-    options.height    = options.height    || defaults.height;
+    if ( cacheService.hasFile(imageId) ){
 
-    return options;
+      winston.info('Server image "%s" from cache', imageId);
+      serveImageCallback(null, imageId);
+
+    } else {
+
+      // See if there is a job pending
+      if ( pendingJobs[imageId] ) {
+
+        winston.info('attach to a pending screenshot job #%s', pendingJobs[imageId].id);
+
+        // just attach to the pending jobs
+        attachListenerTo(pendingJobs[imageId], imageId,  serveImageCallback);
+
+      } else {
+
+        winston.info("create a new job");
+
+        // Create a new job and attach to it.
+        var job = jobs.create('screenshot', { title : url, id : imageId , options : options } ).save();
+        pendingJobs[imageId] = job;
+        attachListenerTo(job, imageId, serveImageCallback);
+
+      }
+    }
+
   };
 
 
   /**
    * Main route.
    */
-  app.get('/', function(req, res, next){
-
-    // Issue a
-    if (!req.query.url) {
-        res.writeHead(404); res.end('At least the URL param must be set.');
-        return;
-    }
-
-    var url = req.query.url;
-
-    // Assume http:// if nothing is specified.
-    if ( url.indexOf('http') === -1 ){
-      url = 'http://' + url;
-    }
-
-    try{
-
-      if (!validator.isURL(url)){
-        throw new Error('Invalid URL: '+ url);
-      }
-
-      // Build options from request.
-      var options   = buildOptions(req.query, config.screenshot);
-
-      var optionStr = JSON.stringify(options);
-
-      // Create a uniqueID from url, options and the image format
-      var image = utils.md5(url + optionStr) + "." + options.format;
-
-      winston.info('Request "%s", cacheId: "%s"', url, image, optionStr);
-
-      // is there a &callback url defined.
-      var responseUrl = options.callback;
-
-      // if there is one, answer with 200 OK instantly
-      if  (!!responseUrl){
-
-        // Assume http:// if nothing is specified.
-        if ( responseUrl.indexOf('http') === -1 ){
-          responseUrl = 'http://' + responseUrl;
+  app.get('/', normalizeReq, function(req, res, next){
+      var serveImage = function(error, id){
+        if ( error ) {
+          next(error);
+          return;
         }
-
-        if (!validator.isURL(responseUrl)){
-          throw new Error('Invalid callback url ' + url);
-        }
-        res.writeHead(200);
-        res.end('Response will be send to '+ responseUrl);
-      }
-
-
-      // &force will invalidate the cache
-      if ( options.force ){
-        winston.info('Remove "%s" from cache', image);
-        cacheService.removeFile(image);
-      }
-
-      var serveImage = function(){
-        cacheService.getFile(image).pipe(res);
+        cacheService.getFile(id).pipe(res);
       };
 
-      if ( cacheService.hasFile(image) ){
+      createOrAttachToJob(req.screenshot.url, req.screenshot.imageId, req.screenshot.options, serveImage);
 
-        winston.info('Server image "%s" from cache', image);
-        serveImage();
-      } else {
+  });
 
-        var attachListenerTo = function(job){
-          job.on('complete', function(){
-            // remove from pending job list.
-            delete pendingJobs[image];
+  app.get('/ajax', normalizeReq, function(req, res, next){
 
-            winston.info("server new image");
-            cacheService.addFile(image);
+    var serverJsonResult = function(error, id){
 
-            serveImage();
-          });
-
-          job.on('failed', function(error){
-            // remove from pending job list.
-            delete pendingJobs[image];
-
-            winston.info('' + error);
-            res.writeHead(503);
-            res.end(''+error);
-          });
-        };
-
-        // See if there is a job pending
-        if ( pendingJobs[image] ) {
-
-          winston.info('attach to a pending screenshot job #%s', pendingJobs[image].id);
-
-          // just attach to the pending jobs
-          attachListenerTo(pendingJobs[image]);
-
-        } else {
-
-          winston.info("create a new job");
-
-          // Create a new job and attach to it.
-          var job = jobs.create('screenshot', { title : url, id : image , options : options } ).save();
-          pendingJobs[image] = job;
-          attachListenerTo(job);
-
-        }
-
+      if ( error ) {
+        next(error);
+        return;
       }
 
+      res.writeHead(200, {"Content-Type" : "application/json"});
 
-    } catch ( error ){
-      winston.info ( '' + error );
-      res.writeHead(503);
-      res.end('' + error);
+      var  domainPort;
+      if ( config.server.port !== "80"){
+        domainPort =  config.server.domain + ':' + config.server.port;
+      } else {
+        domainPort =  config.server.domain;
+      }
+
+      res.end(JSON.stringify({
+        request : "success",
+        url :  domainPort + '/image/' + id
+      }));
+    };
+
+    createOrAttachToJob(req.screenshot.url, req.screenshot.imageId, req.screenshot.options, serverJsonResult);
+  });
+
+
+  app.get('/image/:id', function(req, res, next){
+
+    if ( cacheService.hasFile(req.params.id) ){
+
+      cacheService.getFile(req.params.id).pipe(res);
+
+    }else{
+
+      res.writeHead(404);
+      res.end();
+
     }
   });
+
+
+
 };
